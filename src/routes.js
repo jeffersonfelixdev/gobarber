182358105675b477f346a816ed2ddec4d2dde4f6
Jefferson Felix <jsfelix@gmail.com>
2019-06-29 01:16:42 -0300

    Cadastro de usuários

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-// Teste de criação de usuário
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Jefferson Felix',
-    email: 'jefferson@teste.com',
-    password_hash: 'abcdefg',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
