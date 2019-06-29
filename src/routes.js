832c64672bbb4aee32ed12718ae8f91ac173543e
Jefferson Felix <jsfelix@gmail.com>
2019-06-29 01:05:09 -0300

    Criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,18 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+// Teste de criação de usuário
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Jefferson Felix',
+    email: 'jefferson@teste.com',
+    password_hash: 'abcdefg',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
