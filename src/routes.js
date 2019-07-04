fab04162f1e14387e62e3aa1765ce08fd5576053
Jefferson Felix <jsfelix@gmail.com>
2019-07-04 17:58:27 -0300

    Listagem de prestadores de serviços

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import multer from 'multer';
 import multerConfig from './config/multer';
 
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 
@@ -16,8 +17,8 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
-routes.put('/users', UserController.update);
-
 routes.post('/files', upload.single('file'), FileController.store);
+routes.put('/users', UserController.update);
+routes.get('/providers', ProviderController.index);
 
 export default routes;
